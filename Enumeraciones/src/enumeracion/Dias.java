b921ac8c738124d5881d043d173ea619311eabdd
AngelTravieso <angeltraviesoc@gmail.com>
2022-12-02 18:37:24 -0400

    enumeradores en java

---
+++ Enumeraciones/src/enumeracion/Dias.java
@@ -0,0 +1,13 @@
+package enumeracion;
+
+// para definir un enumerador se cambia la palabra 'class' por 'enum'
+// por default los elementos de un enumerador son public static y finals (constantes en Java)
+public enum Dias {
+    LUNES,
+    MARTES,
+    MIERCOLES,
+    JUEVES,
+    VIERNES,
+    SABADO,
+    DOMINGO,
+}
