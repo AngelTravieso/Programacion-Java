e30b39e84815ee8471d7a5d93d793c4e3a6f1b01
Angel Travieso <angeltraviesoc@gmail.com>
2022-12-07 10:50:19 -0400

    polimorfismo

--- Sobreescritura/src/test/TestSobreescritura.java
+++ Sobreescritura/src/test/TestSobreescritura.java
@@ -1,11 +1,32 @@
 package test;
 
+import domain.*;
 import domain.Gerente;
 
 public class TestSobreescritura {
 
     public static void main(String[] args) {
-        Gerente gerente1 = new Gerente("Angel", 5000, "Sistemas");
-        System.out.println("gerente1 = " + gerente1.obtenerDetalles());
+//        Gerente gerente1 = new Gerente("Angel", 5000, "Sistemas");
+//        System.out.println("gerente1 = " + gerente1.obtenerDetalles());
+
+        Empleado empleado = new Empleado("Juan", 5000);
+//        System.out.println("empleado = " + empleado.obtenerDetalles());
+
+        imprimir(empleado);
+
+        Gerente gerente = new Gerente("Karla", 10000, "Contabilidad");
+//        System.out.println("gerente = " + gerente.obtenerDetalles());
+
+        imprimir(gerente);
+
     }
+    
+    /*
+        polimorfismo, cambia el comportamiento segun el objeto que se
+        pasa como referencia
+    */
+    public static void imprimir(Empleado empleado) {
+        System.out.println("empleado = " + empleado.obtenerDetalles());
+    }
+
 }
