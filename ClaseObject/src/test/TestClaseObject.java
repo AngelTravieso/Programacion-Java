d464325ffedd85179883cf13756bbcdcd9d58926
Angel Travieso <angeltraviesoc@gmail.com>
2022-12-07 14:26:03 -0400

    comparacion de objetos, metodos equals y hashcode

--- ClaseObject/src/test/TestClaseObject.java
+++ ClaseObject/src/test/TestClaseObject.java
@@ -14,5 +14,22 @@ public class TestClaseObject {
         } else {
             System.out.println("Tienen distinta referencia en memoria");
         }
+        
+        // verificar si los objetos son iguales por su contenido con equals
+        /*
+            equals compara no solo la referencia del objeto, sino tambien
+            el contenido
+        */
+        if(empleado1.equals(empleado2)) {
+            System.out.println("Los objetos son iguales en contenido");
+        } else {
+            System.out.println("Los objetos son distintos en contenido");
+        }
+        
+        if(empleado1.hashCode() == empleado2.hashCode()) {
+            System.out.println("El valor hashcode es igual");
+        } else {
+            System.out.println("El valor hashcode es distinto");
+        }
     }
 }
