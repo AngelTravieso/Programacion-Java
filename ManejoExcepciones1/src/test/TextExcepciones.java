b942171ea8e0efda5f5985511c85604e788712f6
Angel Travieso <angeltraviesoc@gmail.com>
2022-12-08 15:10:14 -0400

    manejo de varias excepciones

--- ManejoExcepciones1/src/test/TextExcepciones.java
+++ ManejoExcepciones1/src/test/TextExcepciones.java
@@ -2,24 +2,36 @@ package test;
 
 // importar metodo estatico
 import static aritmetica.Aritmetica.division;
+import excepciones.OperacionExcepcion;
 
 public class TextExcepciones {
 
     public static void main(String[] args) {
         int resultado = 0;
 
-//        try {
+        try {
             resultado = division(10, 0);
-//        } catch (Exception e) {
-//            System.out.println("Ocurrio un error: ");
-//            
-//            // imprimir la pila de excepciones
-//            e.printStackTrace(System.out); // La salida se manda a consola
-//            
-//            // imprimir mensaje de error sin ver la pila de excepciones
-//            System.out.println(e.getMessage());
-//        }
 
-        System.out.println("resultado = " + resultado);
+            // para procesar varias excepciones
+        } catch (OperacionExcepcion e) {
+            System.out.println("Ocurrio un error de tipo OperacionExcepcion");
+            System.out.println(e.getMessage());
+
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error de tipo Exception: ");
+
+            // imprimir la pila de excepciones
+            e.printStackTrace(System.out); // La salida se manda a consola
+
+            // imprimir mensaje de error sin ver la pila de excepciones
+            System.out.println(e.getMessage());
+
+            // siempre se ejecuta este bloque, ocurra o no alguna excepcion
+        } finally {
+            System.out.println("Se reviso la division entre cero");
+        }
+
+        System.out.println(
+                "resultado = " + resultado);
     }
 }
