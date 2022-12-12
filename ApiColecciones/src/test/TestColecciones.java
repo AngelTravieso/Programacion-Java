8f1ea219b85d49c72cadf768bdbea2d116c46dcd
Angel Travieso <angeltraviesoc@gmail.com>
2022-12-12 17:38:56 -0400

    maps (hashmaps) en java

--- ApiColecciones/src/test/TestColecciones.java
+++ ApiColecciones/src/test/TestColecciones.java
@@ -6,7 +6,7 @@ public class TestColecciones {
 
     public static void main(String[] args) {
         // declarar lista
-        List miLista = new ArrayList();
+        List miLista = new ArrayList(); // Lista
 
         // agregar datos a la lista, se puede agregar cualquier tipo Object
         // la lista guarda el orden en el que se agregan
@@ -14,11 +14,11 @@ public class TestColecciones {
         miLista.add("Martes");
         miLista.add("Miercoles");
         miLista.add("Jueves");
-        
+
         // En las listas se pueden duplicar los elementos
         miLista.add("Viernes");
         miLista.add("Viernes");
-        imprimir(miLista);
+//        imprimir(miLista);
 
 //        System.out.println("miLista = " + miLista);
         System.out.println("\n");
@@ -26,16 +26,40 @@ public class TestColecciones {
         // El set no garantiza que se mantenga el orden de los elementos
         // el set es mas rapido porque no maneja un orden
         // no se permiten elementos duplicados
-        Set miSet = new HashSet();
-        
+        Set miSet = new HashSet(); // Set
+
         miSet.add("Viernes");
         miSet.add("Sabado");
         miSet.add("Domingo");
-        
+
         // tomara el ultimo elemento repetido
         miSet.add("Domingo");
 
-        imprimir(miSet);
+//        imprimir(miSet);
+        // Declarar hashmap
+        // son pares clave - valor
+        Map miMapa = new HashMap();
+
+        // agregar elementos
+        miMapa.put("valor1", "Juan");
+        miMapa.put("valor2", "Karla");
+        miMapa.put("valor3", 1234);
+
+        // Acceder al valor
+        /*
+        regresa un tipo object (downcast) hay que hacer casting
+         */
+        String elemento = (String) miMapa.get("valor1");
+//        System.out.println("elemento = " + elemento);
+
+        // Obtener todas las llaves del mapa
+        imprimir(miMapa.keySet());
+        
+        // Obtener todos los valores
+        imprimir(miMapa.values());
+        
+        // Obtener mapa completo (llave: valor)
+        imprimir(miMapa.entrySet());
 
     }
 
